224065d1625543acf1070b3130787337744f29b7
StupidLamer <grisga2001@gmail.com>
2020-11-14 05:54:04 +0300

    add navbar component

---
+++ theory/components/Navbar.vue
@@ -0,0 +1,31 @@
+<template>
+<header>
+  <nav class="navbar navbar-expand-lg navbar-light bg-light">
+  <nuxt-link class="navbar-brand" to="/">Nuxt JS</nuxt-link>
+
+  <div class="navbar-collapse" >
+    <ul class="navbar-nav mr-auto">
+      <li class="nav-item active">
+        <nuxt-link class="nav-link" to="/">Home</nuxt-link>
+      </li>
+      <li class="nav-item">
+        <nuxt-link class="nav-link" to="/users">Users</nuxt-link>
+      </li>
+      <li class="nav-item">
+        <nuxt-link class="nav-link" to="/about">About</nuxt-link>
+      </li>
+    </ul>
+  </div>
+</nav>
+</header>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
