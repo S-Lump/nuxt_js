96a133a821ed9ce0212b8fe0b8664c70050a6639
StupidLamer <grisga2001@gmail.com>
2020-11-14 06:12:53 +0300

    add dynamic routes

---
+++ theory/pages/users/index.vue
@@ -0,0 +1,29 @@
+<template>
+  <div>
+      <h1>Users page</h1>
+      <ul>
+        <li v-for="user of users" :key="user">
+          <a href="#" @click.prevent="goTo(user)">User {{user}}</a>
+        </li>
+      </ul>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      users: [1, 2, 3, 4, 5]
+    }
+  },
+  methods: {
+    goTo(user) {
+      this.$router.push('/users/' + user)
+    }
+  }
+}
+</script>
+
+<style>
+
+</style>
