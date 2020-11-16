f7e305f163e9cab7ff2c2abc36fefdc5217170d7
StupidLamer <grisga2001@gmail.com>
2020-11-16 22:51:16 +0300

    add components nav and post

---
+++ blog/components/main/Post.vue
@@ -0,0 +1,65 @@
+<template>
+  <el-card
+    shadow="hover"
+    :body-style="{padding: 0}"
+    class="post"
+  >
+    <header slot="header" class="post-header">
+      <h3>Post title</h3>
+
+      <small>
+        <i class="el-icon-time"></i>
+        {{ new Date().toLocaleDateString() }}
+      </small>
+    </header>
+    <div class="post-body">
+      <img 
+        src="https://nallihanhaber.com/wp-content/uploads/2020/03/appledan-evde-calisanlarin-isini-kolaylastiracak-oneriler-EprZBAOj.jpg"
+        alt="" 
+        class="post-image"
+      >
+    </div>
+    <footer class="post-footer">
+      <el-button round @click="openPost">Открыть</el-button>
+
+      <span>
+        <i class="el-icon-message"></i>
+        12
+      </span>
+    </footer>
+  </el-card>
+</template>
+
+<script>
+export default {
+  methods: {
+    openPost() {
+      console.log('Open post')
+    }
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+  .post {
+    margin-bottom: 1.5rem;
+  }
+
+  .post-header {
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+  }
+
+  .post-image {
+    width: 100%;
+    height: auto;
+  }
+
+  .post-footer {
+    padding: 1rem;
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+  }
+</style>
