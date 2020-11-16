670f7925973fbee69d7cb6c2b5989b9a5ac0ffed
StupidLamer <grisga2001@gmail.com>
2020-11-16 03:57:25 +0300

    update navbar

--- theory/components/Navbar.vue
+++ theory/components/Navbar.vue
@@ -6,13 +6,19 @@
   <div class="navbar-collapse" >
     <ul class="navbar-nav mr-auto">
       <li class="nav-item active">
-        <nuxt-link class="nav-link" to="/">Home</nuxt-link>
+        <nuxt-link exact active-class="active" class="nav-link" to="/">Home</nuxt-link>
       </li>
       <li class="nav-item">
-        <nuxt-link class="nav-link" to="/users">Users</nuxt-link>
+        <nuxt-link no-prefetch active-class="active" class="nav-link" to="/users">Users</nuxt-link>
       </li>
       <li class="nav-item">
-        <nuxt-link class="nav-link" to="/about">About</nuxt-link>
+        <nuxt-link no-prefetch active-class="active" class="nav-link" to="/about">About</nuxt-link>
+      </li>
+      <li class="nav-item">
+        <nuxt-link no-prefetch active-class="active" class="nav-link" to="/login">Login</nuxt-link>
+      </li>
+      <li v-if="isAuth" class="nav-item">
+        <a class="nav-link" href="#" @click.prevent="logout">Logout</a>
       </li>
     </ul>
   </div>
@@ -22,7 +28,16 @@
 
 <script>
 export default {
-
+  computed: {
+    isAuth() {
+      return this.$store.getters.isAuth
+    }
+  },
+  methods: {
+    logout() {
+      this.$store.dispatch('logout')
+    }
+  }
 }
 </script>
 
