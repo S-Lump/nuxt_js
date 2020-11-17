57237200177faa48d1196e55276abe3848bfaa91
StupidLamer <grisga2001@gmail.com>
2020-11-18 02:14:29 +0300

    add method to update post

--- blog/pages/admin/post/_id.vue
+++ blog/pages/admin/post/_id.vue
@@ -1,19 +1,105 @@
 <template>
-  <div>
-    <el-breadcrumb separator="/">
+  <div class="page-wrap">
+    <el-breadcrumb separator="/" class="mb">
       <el-breadcrumb-item to="/admin/list">Посты</el-breadcrumb-item>
-      <el-breadcrumb-item>{{$route.params.id}}</el-breadcrumb-item>
+      <el-breadcrumb-item>{{ post.title }}</el-breadcrumb-item>
     </el-breadcrumb>
+
+    <el-form 
+      :model="controls" 
+      :rules="rules" 
+      ref="form"
+      @submit.native.prevent="onSubmit">
+
+      <!-- <h2>Войти в панель администратора</h2> -->
+
+      <el-form-item label="Текст в формате .md или .html" prop="text">
+        <el-input 
+          v-model.trim="controls.text"
+          type="textarea"
+          resize="none"
+          :rows="10"></el-input>
+      </el-form-item>
+
+      <div class="mb">
+        <small>
+          <i class="el-icon-time"></i>
+          <span style="margin-left: 10px">
+            {{ new Date(post.date).toLocaleString() }}
+          </span>
+        </small>
+      </div>
+
+      <el-form-item>
+        <el-button 
+          type="primary" 
+          round
+          native-type="submit"
+          :loading="loading">
+          Обновить
+        </el-button>
+      </el-form-item>
+    </el-form>
   </div>
 </template>
 
 <script>
 export default {
   layout: 'admin',
-  middleware: ['admin-auth']
+  middleware: ['admin-auth'],
+  head() {
+    return {
+      title: this.post.title
+    }
+  },
+  validate({params}) {
+    return !!params.id
+  },
+  async asyncData({store, params}) {
+    const post = await store.dispatch('post/fetchAdminById', params.id)
+    return {post} 
+  },
+  data() {
+    return {
+      loading: false,
+      controls: {
+        text: '',
+        password: ''
+      },
+      rules: {
+        text: [
+          { required: true, message: 'Текст не должен быть пустым', trigger: 'blur' },
+        ]
+      }
+    }
+  },
+  methods: {
+    onSubmit() {
+      this.$refs.form.validate(async valid => {
+        if (valid) {
+          this.loading = false
+
+          const formData = {
+            text: this.controls.text,
+            id: this.post._id
+          }
+
+          try {
+            await this.$store.dispatch('post/update', formData)
+            this.$message.success('Пост обновлен') 
+            this.loading = false                 
+          } catch(e) {
+            this.loading = false  
+          } 
+        }
+      })
+    }
+  }
 }
 </script>
 
-<style>
-
+<style lang="scss" scoped>
+  .page-wrap {
+    max-width: 600px;
+  }
 </style>
--- blog/store/post.js
+++ blog/store/post.js
@@ -1,27 +1,39 @@
+const posts = [
+  {
+    title: 'Post 1',
+    date: new Date(), 
+    views: 22,
+    comments: [1, 2],
+    _id: 'id1'
+  },
+  {
+    title: 'Post 2',
+    date: new Date(), 
+    views: 22,
+    comments: [1, 2],
+    _id: 'id2'
+  }
+]
+
 export const actions = {
   async fetchAdminPosts({}) {
     return await new Promise(resolve => {
       setTimeout(() => {
-        resolve([
-          {
-            title: 'Post 1',
-            date: new Date(), 
-            views: 22,
-            comments: [1, 2],
-            _id: Math.random()
-          },
-          {
-            title: 'Post 2',
-            date: new Date(), 
-            views: 22,
-            comments: [1, 2],
-            _id: Math.random()
-          }
-        ])
+        resolve(posts)
       }, 1000)
     })
   },
   async remove({}, id) {
     console.log('deleted')
+  },
+  async update({}, {id, text}) {
+    console.log('updated')
+  },
+  async fetchAdminById({}, id) {
+    return await new Promise(resolve => {
+      setTimeout(() => {
+        resolve(posts.find(p => p._id === id))
+      }, 1000)
+    })
   }
 }
