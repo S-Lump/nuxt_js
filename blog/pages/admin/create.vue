bae608b60a5fb4d3dfd16df1b151ca2d51ff3779
StupidLamer <grisga2001@gmail.com>
2020-11-18 03:15:35 +0300

    add upload component

--- blog/pages/admin/create.vue
+++ blog/pages/admin/create.vue
@@ -30,6 +30,18 @@
         </div>
       </el-dialog>
 
+      <el-upload
+        class="mb"
+        drag
+        ref="upload"
+        action="https://jsonplaceholder.typicode.com/posts/"
+        :on-change="handleImageChange"
+        :auto-upload="false">
+        <i class="el-icon-upload"></i>
+        <div class="el-upload__text">Перетащите картинку или <em>нажмите</em></div>
+        <div class="el-upload__tip" slot="tip">Только jpg/png</div>
+      </el-upload>
+
       <el-form-item>
         <el-button 
           type="primary" 
@@ -49,6 +61,7 @@ export default {
   middleware: ['admin-auth'],
   data() {
     return {
+      image: null,
       previewDialog: false,
       loading: false,
       controls: {
@@ -68,27 +81,34 @@ export default {
   methods: {
     onSubmit() {
       this.$refs.form.validate(async valid => {
-        if (valid) {
+        if (valid && this.image) {
           this.loading = true
 
           const formData = {
             title: this.controls.title,
-            text: this.controls.text
+            text: this.controls.text,
+            image: this.image
           }
 
           try {
             await this.$store.dispatch('post/create', formData)
             this.controls.text = ''
             this.controls.title = ''
+            this.image = null
+            this.$refs.upload.clearFiles()
             this.$message.success('Пост создан')
           } catch(e) {
 
           } finally {
             this.loading = false
-          }
-          
+          }  
+        } else {
+          this.$message.warning('Данные введены некорректно')
         }
       })
+    },
+    handleImageChange(file, fileList) {
+      this.image = file.raw
     }
   }
 }
